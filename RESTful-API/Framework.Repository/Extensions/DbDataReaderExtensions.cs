f0d2c2d766d69ec4853d552d33ca57a7d54259cf
Ismael <jsalinas@e-coffeetech.com>
2018-02-11 10:39:34 -0800

    Extensiones para convertir un DataReader a un tipo en especifico o un listado de ese tipo.

---
+++ RESTful-API/Framework.Repository/Extensions/DbDataReaderExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+
+namespace Framework.Repository.Extensions
+{
+    public static class DbDataReaderExtensions
+    {
+        public static IList<T> MapToList<T>(this DbDataReader dataReader)
+        {
+            if (dataReader == null) throw new ArgumentNullException(nameof(dataReader));
+
+            var list = new List<T>();
+            var properties = typeof(T).GetRuntimeProperties();
+
+            var columns = dataReader.GetColumnSchema()
+                .Where(x => properties.Any(y =>
+                    string.Equals(y.Name, x.ColumnName, StringComparison.CurrentCultureIgnoreCase)))
+                .ToDictionary(key => key.ColumnName);
+
+            if (!dataReader.HasRows) return list;
+
+            var propertyInfos = properties as IList<PropertyInfo> ?? properties.ToList();
+            while (dataReader.Read())
+            {
+                var instance = Activator.CreateInstance<T>();
+                foreach (var propertyInfo in propertyInfos)
+                {
+                    var columnOrdinal = columns[propertyInfo.Name].ColumnOrdinal;
+                    if (columnOrdinal == null) continue;
+                    var val = dataReader.GetValue(columnOrdinal.Value);
+                    propertyInfo.SetValue(instance, val == DBNull.Value ? null : val);
+                }
+                list.Add(instance);
+            }
+            return list;
+        }
+
+        public static T? MapToValue<T>(this DbDataReader dataReader) where T : struct
+        {
+            if (dataReader == null) throw new ArgumentNullException(nameof(dataReader));
+
+            if (!dataReader.HasRows) return new T?();
+
+            if (dataReader.Read())
+                return dataReader.IsDBNull(0) ? new T?() : dataReader.GetFieldValue<T>(0);
+
+            return new T?();
+        }
+    }
+}
