64b4e850b74d0bcf16718667ba3f730d672bee2a
Ismael <jsalinas@e-coffeetech.com>
2018-02-11 10:43:15 -0800

    Contrato para dar soporte a procedimientos almacenados.

---
+++ RESTful-API/Framework.Repository/Contract/IStoredProcedure.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+
+namespace Framework.Repository.Contract
+{
+    public interface IStoredProcedure
+    {
+        DbCommand StoredProcedure(string name, bool defaultSchema = true);
+    }
+}
