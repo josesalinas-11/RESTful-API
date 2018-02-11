8677708b9317b487ea8a0199af54588a32b90390
Ismael <jsalinas@e-coffeetech.com>
2018-02-11 11:40:08 -0800

    Contexto base para que da soporte a soft delete y a loggs.

---
+++ RESTful-API/Framework.EntityFrameworkRepository/Context/DataContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Framework.Data.Entity.Contract;
+using Framework.EntityFrameworkRepository.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Framework.EntityFrameworkRepository.Context
+{
+    public abstract class DataContext : DbContext
+    {
+        protected DataContext(DbContextOptions<DataContext> options) : base(options) =>
+#if DEBUG
+            this.ConfigureLogging(l => Debug.WriteLine(l));
+#endif
+
+        protected DataContext() =>
+#if DEBUG
+            this.ConfigureLogging(l => Debug.WriteLine(l));
+#endif
+
+
+        public Func<DateTime> TimestampProvider { get; set; } = () => DateTime.UtcNow;
+
+        public override int SaveChanges()
+        {
+            TrackChanges();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            TrackChanges();
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void TrackChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified || e.State == EntityState.Deleted))
+            {
+                if (!(entry.Entity is ISoftDelete auditable)) continue;
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        break;
+                    case EntityState.Deleted:
+                        auditable.DeleteAt = TimestampProvider();
+                        entry.State = EntityState.Modified;
+                        break;
+                    case EntityState.Detached:
+                        break;
+                    case EntityState.Unchanged:
+                        break;
+                    case EntityState.Modified:
+                        break;
+                }
+            }
+        }
+    }
+}
