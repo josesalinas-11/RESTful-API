e352f758738029268ba08e689f53cff651df4b6f
Ismael <jsalinas@e-coffeetech.com>
2018-02-10 19:18:02 -0800

    Se agrego el contrato para poder comprobar que las propiedades que se pasan de forma dinámica correspondan a los modelos.

---
+++ RESTful-API/Framework.PropertyMapper/Contract/IPropertyMapperService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Framework.PropertyMapper.Contract
+{
+    public interface IPropertyMapperService
+    {
+        bool ValidMappingExistsFor<TSource, TDestination>(string fields);
+
+        Dictionary<string, PropertyMapperValue> GetPropertyMapper<TSource, TDestination>();
+    }
+}
