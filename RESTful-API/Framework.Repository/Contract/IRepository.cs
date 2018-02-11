637a5abec7831a7d8ddd06a0c3e15c8f10d3451d
Ismael <jsalinas@e-coffeetech.com>
2018-02-11 10:50:14 -0800

    Contrato para implementar un repositorio con las operaciones de un CRUD.

---
+++ RESTful-API/Framework.Repository/Contract/IRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Framework.Repository.Contract
+{
+    public interface IRepositoryIRepository<TEntity> : IRepositoryReadOnly<TEntity>, IStoredProcedure
+        where TEntity : class
+    {
+
+        void Insert(TEntity entity);
+
+        void InsertRange(IEnumerable<TEntity> entities);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void Delete(Expression<Func<TEntity, bool>> predicate);
+
+        #region Async
+
+        Task<int> InsertAsync(TEntity entity);
+
+        Task<int> InsertRangeAsync(IEnumerable<TEntity> entities);
+
+        Task<int> UpdateAsync(TEntity entity);
+
+        Task<int> DeleteAsync(TEntity entity);
+
+        Task<int> DeleteAsync(Expression<Func<TEntity, bool>> predicate);
+
+        #endregion
+
+    }
+}
