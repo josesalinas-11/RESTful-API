811e070b222b7a8c9b77f3b442d810cac2d87b39
Ismael <jsalinas@e-coffeetech.com>
2018-02-11 11:33:25 -0800

    Se agregaron dos nuevos contratos

--- RESTful-API/Framework.Repository/Contract/IRepositoryReadOnly.cs
+++ RESTful-API/Framework.Repository/Contract/IRepositoryReadOnly.cs
@@ -8,12 +8,16 @@ namespace Framework.Repository.Contract
 {
     public interface IRepositoryReadOnly<TEntity> where TEntity : class
     {
-        PaginatedList<TEntity> Paginate(int pageNumber, int pageSize, Expression<Func<TEntity, bool>> predicate, string orderBy, Dictionary<string, PropertyMapperValue> propertyMappingValues, bool showDeleted = false, params Expression<Func<TEntity, object>>[] includeProperties);
+        PaginatedList<TEntity> Paginate(int pageNumber, int pageSize, Expression<Func<TEntity, bool>> predicate, string orderBy, Dictionary<string, PropertyMapperValue> propertyMapperValues, bool showDeleted = false, params Expression<Func<TEntity, object>>[] includeProperties);
 
-        PaginatedList<TResult> Paginate<TResult>(int pageNumber, int pageSize, Expression<Func<TEntity, bool>> predicate, Func<TEntity, TResult> selector, string orderBy, Dictionary<string, PropertyMapperValue> propertyMappingValues, bool showDeleted, params Expression<Func<TEntity, object>>[] includeProperties);
+        PaginatedList<TResult> Paginate<TResult>(int pageNumber, int pageSize, Expression<Func<TEntity, bool>> predicate, Func<TEntity, TResult> selector, string orderBy, Dictionary<string, PropertyMapperValue> propertyMapperValues, bool showDeleted, params Expression<Func<TEntity, object>>[] includeProperties);
 
         TEntity GetSingle(Expression<Func<TEntity, bool>> predicate, bool showDeleted = false);
 
         TResult GetSingle<TResult>(Expression<Func<TEntity, bool>> predicate, Func<TEntity, TResult> selector, bool showDeleted = false) where TResult : class;
+
+        TEntity GetSingleIncluding(Expression<Func<TEntity, bool>> predicate, bool showDeleted = false, params Expression<Func<TEntity, object>>[] includeProperties);
+
+        TResult GetSingleIncluding<TResult>(Expression<Func<TEntity, bool>> predicate, Func<TEntity, TResult> selector, bool showDeleted = false, params Expression<Func<TEntity, object>>[] includeProperties) where TResult : class;
     }
 }
