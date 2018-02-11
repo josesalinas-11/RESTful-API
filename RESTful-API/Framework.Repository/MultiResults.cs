647782bb3f8483a60b375a27277deeba74def115
Ismael <jsalinas@e-coffeetech.com>
2018-02-11 10:44:26 -0800

    Permite que un procedimiento almacenado pueda retornar multiplex resultados tipados.

---
+++ RESTful-API/Framework.Repository/MultiResults.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+using Framework.Repository.Extensions;
+
+namespace Framework.Repository
+{
+    public class MultiResults
+    {
+        private readonly DbDataReader _dataReader;
+
+        public MultiResults(DbDataReader dataReader)
+        {
+            _dataReader = dataReader ?? throw new ArgumentNullException(nameof(dataReader));
+        }
+
+        public IList<T> ReadToList<T>()
+        {
+            return _dataReader.MapToList<T>();
+        }
+
+        public T? ReadToValue<T>() where T : struct
+        {
+            return _dataReader.MapToValue<T>();
+        }
+
+        public Task<bool> NextResultAsync()
+        {
+            return _dataReader.NextResultAsync();
+        }
+
+        public Task<bool> NextResultAsync(CancellationToken cancellationToken)
+        {
+            return _dataReader.NextResultAsync(cancellationToken);
+        }
+
+        public bool NextResult()
+        {
+            return _dataReader.NextResult();
+        }
+    }
+}
